0fd6aeb416c68a82d20f86f4f608e0b8eef29d09
fedarovich <fedarovich@localhost>
2015-03-03 18:12:14 +0300

    Added some of Lecture 02 Projects

---
+++ WpfTraining/Lecture02/RoutedEvents/EventViewModel.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace RoutedEvents
+{
+    public class EventViewModel
+    {
+        public EventViewModel(object sender, RoutedEventArgs e)
+        {
+            Sender = sender != null ? sender.ToString() : null;
+            Source = e.Source != null ? e.Source.ToString() : null;
+            OriginalSource = e.OriginalSource != null ? e.OriginalSource.ToString() : null;
+            Handled = e.Handled;
+        }
+
+        public string Sender { get; private set; }
+
+        public string Source { get; private set; }
+
+        public string OriginalSource { get; private set; }
+
+        public bool Handled { get; private set; }
+    }
+}
