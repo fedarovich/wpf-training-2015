a7ffdc1ea81570830b28988fe9617ed73b79bd45
fedarovich <p.fedarovich@sam-solutions.com>
2015-03-03 16:48:58 +0300

    Added Lecture 01 Projects.

---
+++ WpfTraining/Lecture01/HelloWorld/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        [STAThread]
+        static void Main(string[] args)
+        {
+            var window = new Window();
+            window.Content = "Hello, world!";
+            var application = new Application();
+            application.Run(window);
+        }
+    }
+}
