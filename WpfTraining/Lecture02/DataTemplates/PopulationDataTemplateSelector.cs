69bfa3f758ffe2af3dca11eaf68c5f95ad30e437
fedarovich <p.fedarovich@sam-solutions.com>
2015-03-10 17:30:12 +0300

    Added more Lecture 02 Projects. Added Common project with Population XML.

---
+++ WpfTraining/Lecture02/DataTemplates/PopulationDataTemplateSelector.cs
@@ -0,0 +1,32 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using Common;
+
+namespace DataTemplates
+{
+    public class PopulationDataTemplateSelector : DataTemplateSelector
+    {
+        public DataTemplate HighPopulationDensityTemplate { get; set; }
+
+        public DataTemplate NormalPopulationDensityTemplate { get; set; }
+
+        public double HighDensityLimit { get; set; }
+
+        public override DataTemplate SelectTemplate(object item, DependencyObject container)
+        {
+            var country = item as CountryViewModel;
+            if (country != null)
+            {
+                if (country.PopulationDensity > HighDensityLimit)
+                {
+                    return HighPopulationDensityTemplate;
+                }
+                else
+                {
+                    return NormalPopulationDensityTemplate;
+                }
+            }
+            return base.SelectTemplate(item, container);
+        }
+    }
+}
