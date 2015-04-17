a4d9fb0ff38f09ead8a579d5725fe31fbbe6f08c
fedarovich <fedarovich@localhost>
2015-04-17 14:41:25 +0300

    Added CustomMarkupExtensions project for Lecture 04.

---
+++ WpfTraining/Lecture04/CustomMarkupExtensions/DynLocExtension.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Markup;
+
+namespace CustomMarkupExtensions
+{
+    public class DynLocExtension : MarkupExtension
+    {
+        private readonly string _key;
+
+        public DynLocExtension(string key)
+        {
+            _key = key;
+        }
+
+        public override object ProvideValue(IServiceProvider serviceProvider)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ WpfTraining/Lecture04/CustomMarkupExtensions/LocalizationManager.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+
+namespace CustomMarkupExtensions
+{
+    public static class LocalizationManager
+    {
+        private static CultureInfo _currentUiCulture = CultureInfo.GetCultureInfo("en-US");
+
+        public static IEnumerable<CultureInfo> Cultures
+        {
+            get
+            {
+                yield return CultureInfo.GetCultureInfo("en-US");
+                yield return CultureInfo.GetCultureInfo("de-DE");
+            }
+        }
+
+        public static CultureInfo CurrentUICulture
+        {
+            get { return _currentUiCulture; }
+            set
+            {
+                _currentUiCulture = value;
+                CurrentUICultureChanged(null, EventArgs.Empty);
+            }
+        }
+
+        public static event EventHandler CurrentUICultureChanged = delegate {};
+    }
+}
