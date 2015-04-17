c8aeefc8dd499a9f39558a5715a80e6fd8b0c326
fedarovich <p.fedarovich@sam-solutions.com>
2015-04-17 14:41:25 +0300

    Added CustomMarkupExtensions project for Lecture 04.

---
+++ WpfTraining/Lecture04/CustomMarkupExtensions/LocExtension.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Markup;
+
+namespace CustomMarkupExtensions
+{
+    [MarkupExtensionReturnType(typeof(string))]
+    public class LocExtension : MarkupExtension
+    {
+        private readonly string key;
+
+        public LocExtension(string key)
+        {
+            this.key = key;
+        }
+
+        public string Culture { get; set; }
+
+        public override object ProvideValue(IServiceProvider serviceProvider)
+        {
+            var culture = string.IsNullOrWhiteSpace(Culture) ? null : CultureInfo.GetCultureInfo(Culture);
+            return Properties.Resources.ResourceManager.GetString(key, culture);
+        }
+    }
+}
