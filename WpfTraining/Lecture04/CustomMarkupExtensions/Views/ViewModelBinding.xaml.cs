4e1e0c442462739531c2e1240342939dde38eaf6
Pavel <p.fedarovich@sam-solutions.com>
2015-04-25 22:59:14 +0300

    Added sample for ViewModelBinding.

---
+++ WpfTraining/Lecture04/CustomMarkupExtensions/Views/ViewModelBinding.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+
+namespace CustomMarkupExtensions.Views
+{
+    /// <summary>
+    /// Interaction logic for ViewModelBinding.xaml
+    /// </summary>
+    public partial class ViewModelBinding : UserControl
+    {
+        public ViewModelBinding()
+        {
+            InitializeComponent();
+        }
+    }
+}
