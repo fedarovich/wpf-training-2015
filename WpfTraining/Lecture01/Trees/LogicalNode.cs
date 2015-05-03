137ed1d2ff1e6726930849ea9198fbf1efb1c52d
Pavel <p.fedarovich@sam-solutions.com>
2015-05-03 14:16:58 +0300

    Added Trees project.

---
+++ WpfTraining/Lecture01/Trees/LogicalNode.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Windows;
+
+namespace Trees
+{
+    public class LogicalNode : TreeNode
+    {
+        private readonly object element;
+
+        public LogicalNode(object element)
+        {
+            this.element = element;
+        }
+
+        public string Name
+        {
+            get
+            {
+                string name = this.element.GetType().Name;
+                var element = this.element as FrameworkElement;
+                if (element != null && !string.IsNullOrEmpty(element.Name))
+                {
+                    name += " [Name = " + element.Name + "]";
+                }
+                return name;
+            }
+        }
+
+        public override IEnumerable<TreeNode> Children
+        {
+            get
+            {
+                var dependencyObject = element as DependencyObject;
+                return dependencyObject != null ?
+                    LogicalTreeHelper.GetChildren(dependencyObject).Cast<object>().Select(x => new LogicalNode(x)) :
+                    Enumerable.Empty<LogicalNode>();
+            }
+        }
+
+        public object Element
+        {
+            get { return element; }
+        }
+    }
+}
