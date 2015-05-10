12314ffb470c4d3d74697a1de853ec236032d08a
Pavel <Pavel@localhost>
2015-05-10 22:24:33 +0300

    Added two more Lecture 05 samples.

---
+++ WpfTraining/Lecture05/ButtonTemplateWithTriggers/MainWindow.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System.Windows;
+
+namespace ButtonTemplateWithTriggers
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void OnMyButtonClick(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Clicked");
+        }
+    }
+}
