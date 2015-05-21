c83cb68fc770828bc42664115beed708f3d5edf7
Pavel <Pavel@localhost>
2015-05-22 00:35:12 +0300

    Added some samples for Blend behaviors.

---
+++ WpfTraining/Lecture07/BlendBehaviors/Views/IntegerInputView.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Windows.Controls;
+
+namespace BlendBehaviors.Views
+{
+    /// <summary>
+    /// Interaction logic for IntegerInputView.xaml
+    /// </summary>
+    public partial class IntegerInputView : UserControl
+    {
+        public IntegerInputView()
+        {
+            InitializeComponent();
+        }
+
+        public IList<CultureInfo> AllCultures
+        {
+            get
+            {
+                return CultureInfo.GetCultures(CultureTypes.AllCultures)
+                    .OrderBy(x => x.Name)
+                    .ToList();
+            }
+        }
+    }
+}
