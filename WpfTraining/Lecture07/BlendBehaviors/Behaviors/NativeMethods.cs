8277fb31ece942fb55e43759da9c46078cc78c9b
fedarovich <fedarovich@localhost>
2015-05-21 18:58:42 +0300

    Added some Lecture 07 projects.

---
+++ WpfTraining/Lecture07/BlendBehaviors/Behaviors/NativeMethods.cs
@@ -0,0 +1,39 @@
+﻿using System.Runtime.InteropServices;
+using System.Text;
+
+namespace BlendBehaviors.Behaviors
+{
+    /// <summary>
+    /// Provides native methods.
+    /// </summary>
+    internal static class NativeMethods
+    {
+        // ReSharper disable InconsistentNaming
+
+        internal enum MapType : uint
+        {
+            MAPVK_VK_TO_VSC = 0x0,
+            MAPVK_VSC_TO_VK = 0x1,
+            MAPVK_VK_TO_CHAR = 0x2,
+            MAPVK_VSC_TO_VK_EX = 0x3,
+        }
+
+        // ReSharper restore InconsistentNaming
+
+        [DllImport("user32.dll")]
+        internal static extern int ToUnicode(
+            uint wVirtKey,
+            uint wScanCode,
+            byte[] lpKeyState,
+            [Out, MarshalAs(UnmanagedType.LPWStr, SizeParamIndex = 4)] 
+            StringBuilder pwszBuff,
+            int cchBuff,
+            uint wFlags);
+
+        [DllImport("user32.dll")]
+        internal static extern bool GetKeyboardState(byte[] lpKeyState);
+
+        [DllImport("user32.dll")]
+        internal static extern uint MapVirtualKey(uint uCode, MapType uMapType);
+    }
+}
---
+++ WpfTraining/Lecture07/BlendBehaviors/Behaviors/TextBoxIntegerInputBehavior.cs
@@ -0,0 +1,137 @@
+﻿using System.Globalization;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+using System.Windows.Interactivity;
+
+namespace BlendBehaviors.Behaviors
+{
+    /// <summary>
+    /// Filters the keyboard input to <see cref="TextBox"/> in order to allow users to enter only correct 
+    /// <see cref="long"/> values.
+    /// </summary>
+    public class TextBoxIntegerInputBehavior : Behavior<TextBox>
+    {
+        #region Constants
+
+        /// <summary>
+        /// The dependency property for the <see cref="AllowNegative" /> property.
+        /// </summary>
+        public static readonly DependencyProperty AllowNegativeProperty =
+            DependencyProperty.Register("AllowNegative", typeof (bool), typeof (TextBoxIntegerInputBehavior),
+                new PropertyMetadata(true));
+
+        /// <summary>
+        /// The dependency property for the <see cref="Culture" /> property.
+        /// </summary>
+        public static readonly DependencyProperty CultureProperty =
+            DependencyProperty.Register("Culture", typeof (CultureInfo), typeof (TextBoxIntegerInputBehavior),
+                new PropertyMetadata(null));
+
+        #endregion
+
+        #region Properties
+
+        /// <summary>
+        /// Gets or sets a value indicating whether negative values are accepted.
+        /// </summary>
+        public bool AllowNegative
+        {
+            get { return (bool) GetValue(AllowNegativeProperty); }
+            set { SetValue(AllowNegativeProperty, value); }
+        }
+
+        /// <summary>
+        /// Gets or sets the number culture. If not set, <see cref="CultureInfo.CurrentCulture" /> will be used.
+        /// </summary>
+        public CultureInfo Culture
+        {
+            get { return (CultureInfo) GetValue(CultureProperty); }
+            set { SetValue(CultureProperty, value); }
+        }
+
+        #endregion
+
+        #region Methods
+
+        /// <summary>
+        /// Invoked after the behavior is attached to an AssociatedObject.
+        /// </summary>
+        /// <remarks>Override this to hook up functionality to the AssociatedObject.</remarks>
+        protected override void OnAttached()
+        {
+            AssociatedObject.PreviewTextInput += OnPreviewTextInput;
+            AssociatedObject.PreviewKeyDown += OnPreviewKeyDown;
+            DataObject.AddPastingHandler(AssociatedObject, OnPasting);
+        }
+
+        /// <summary>
+        /// Invoked when the behavior is being detached from its AssociatedObject, but before it has actually occurred.
+        /// </summary>
+        /// <remarks>Override this to unhook functionality from the AssociatedObject.</remarks>
+        protected override void OnDetaching()
+        {
+            AssociatedObject.PreviewTextInput -= OnPreviewTextInput;
+            AssociatedObject.PreviewKeyDown -= OnPreviewKeyDown;
+            DataObject.RemovePastingHandler(AssociatedObject, OnPasting);
+        }
+
+        private void OnPreviewTextInput(object sender, TextCompositionEventArgs e)
+        {
+            e.Handled = !IsValidInput(e.Text);
+        }
+
+        private void OnPasting(object sender, DataObjectPastingEventArgs e)
+        {
+            if (!e.SourceDataObject.GetDataPresent(DataFormats.Text, true))
+            {
+                e.CancelCommand();
+            }
+            else
+            {
+                var text = (string) e.SourceDataObject.GetData(DataFormats.Text, true);
+                if (!IsValidInput(text))
+                {
+                    e.CancelCommand();
+                }
+            }
+        }
+
+        private bool IsValidInput(string inputText)
+        {
+            // Concatenate the text before selection, the input text and the text after selection to get the resulting text to validate.
+            var prevText = AssociatedObject.Text;
+            var prevStart = prevText.Substring(0, AssociatedObject.SelectionStart);
+            var prevEnd = prevText.Substring(AssociatedObject.SelectionStart + AssociatedObject.SelectionLength);
+            var newText = string.Concat(prevStart, inputText, prevEnd);
+            // Allow text containing only "-" to simplify entering negative values.
+            if (newText == "-" && AllowNegative)
+            {
+                return true;
+            }
+            var culture = Culture ?? CultureInfo.CurrentCulture;
+            long result;
+            return long.TryParse(newText, GetNumberStyles(), culture, out result);
+        }
+
+        private void OnPreviewKeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Space)
+            {
+                e.Handled = true;
+            }
+        }
+
+        private NumberStyles GetNumberStyles()
+        {
+            var styles = NumberStyles.None;
+            if (AllowNegative)
+            {
+                styles |= NumberStyles.AllowLeadingSign;
+            }
+            return styles;
+        }
+
+        #endregion
+    }
+}
