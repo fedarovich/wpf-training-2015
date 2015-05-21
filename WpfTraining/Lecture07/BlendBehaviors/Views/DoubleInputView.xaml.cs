e0686631631d62b4299a37e832edaa8e13acf8ea
Pavel <p.fedarovich@sam-solutions.com>
2015-05-22 00:35:12 +0300

    Added some samples for Blend behaviors.

---
+++ WpfTraining/Lecture07/BlendBehaviors/Views/DoubleInputView.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Windows.Controls;
+using BlendBehaviors.Behaviors;
+
+namespace BlendBehaviors.Views
+{
+    /// <summary>
+    /// Interaction logic for DoubleInputView.xaml
+    /// </summary>
+    public partial class DoubleInputView : UserControl
+    {
+        public DoubleInputView()
+        {
+            InitializeComponent();
+        }
+
+        public IList<CultureInfo> AllCultures
+        {
+            get
+            {
+                return CultureInfo.GetCultures(CultureTypes.AllCultures)
+                    .OrderBy(x => x.Name)
+                    .ToList();
+            }
+        }
+
+        public IList<DecimalKeyInputMode> DecimalKeyInputModes
+        {
+            get { return new[] {DecimalKeyInputMode.DecimalSeparator, DecimalKeyInputMode.Native}; }
+        }
+    }
+}
