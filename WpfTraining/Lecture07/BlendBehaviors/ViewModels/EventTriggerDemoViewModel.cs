bc0463828bb747060f4e904f545ad2bcb40dfd71
Pavel <p.fedarovich@sam-solutions.com>
2015-05-23 18:19:52 +0300

    Added more Lecture 07 projects.

---
+++ WpfTraining/Lecture07/BlendBehaviors/ViewModels/EventTriggerDemoViewModel.cs
@@ -0,0 +1,38 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Windows;
+using System.Windows.Input;
+using JetBrains.Annotations;
+using Microsoft.Expression.Interactivity.Core;
+
+namespace BlendBehaviors.ViewModels
+{
+    public class EventTriggerDemoViewModel : INotifyPropertyChanged
+    {
+        public EventTriggerDemoViewModel()
+        {
+            SayHelloCommand = new ActionCommand(
+                () => MessageBox.Show("Hello from view model command."));
+        }
+
+        public void SayHello()
+        {
+            MessageBox.Show("Hello from view model method.");
+        }
+
+        public ICommand SayHelloCommand { get; private set; }
+
+        #region INotifyPropertyChanged
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        [NotifyPropertyChangedInvocator]
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChangedEventHandler handler = PropertyChanged;
+            if (handler != null) handler(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        #endregion
+    }
+}
