77beda8554e06e3e524d182b4602355b6b8c0d19
Pavel <p.fedarovich@sam-solutions.com>
2015-05-23 23:47:55 +0300

    Added more Lecture 07 projects (weak events, rich text).

---
+++ WpfTraining/Lecture07/WeakEvents/Handlers/HandlerBase.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Threading.Tasks;
+using System.Windows;
+using JetBrains.Annotations;
+
+namespace WeakEvents.Handlers
+{
+    public abstract class HandlerBase : INotifyPropertyChanged
+    {
+        private bool notified;
+
+        public bool Notified
+        {
+            get { return notified; }
+            private set
+            {
+                if (value.Equals(notified)) return;
+                notified = value;
+                RaisePropertyChanged();
+            }
+        }
+
+        protected async void Notify()
+        {
+            Notified = true;
+            await Task.Delay(500);
+            Notified = false;
+        }
+
+        #region INotifyPropertyChanged
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        [NotifyPropertyChangedInvocator]
+        protected virtual void RaisePropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            var handler = PropertyChanged;
+            if (handler != null) 
+                handler(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        #endregion
+    }
+}
---
+++ WpfTraining/Lecture07/WeakEvents/MainWindow.xaml.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using WeakEvents.Handlers;
+
+namespace WeakEvents
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private readonly ObservableCollection<StrongHandler> strongHandlers = 
+            new ObservableCollection<StrongHandler>();
+
+        private readonly ObservableCollection<WeakHandler> weakHandlers =
+            new ObservableCollection<WeakHandler>();
+
+        private readonly ObservableCollection<GenericWeakHandler> genericWeakHandlers =
+            new ObservableCollection<GenericWeakHandler>(); 
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        public IList<StrongHandler> StrongHandlers
+        {
+            get { return strongHandlers; }
+        }
+
+        public IList<WeakHandler> WeakHandlers
+        {
+            get { return weakHandlers; }
+        }
+
+        public IList<GenericWeakHandler> GenericWeakHandlers
+        {
+            get { return genericWeakHandlers; }
+        }
+
+        private void OnCollect(object sender, RoutedEventArgs e)
+        {
+            GC.Collect();
+            GC.WaitForPendingFinalizers();
+            GC.Collect();
+        }
+
+        private void OnAddStrongHandler(object sender, RoutedEventArgs e)
+        {
+            var handler = new StrongHandler();
+            NotifyButton.Click += handler.OnClick;
+            strongHandlers.Add(handler);
+        }
+
+        private void OnAddWeakHandler(object sender, RoutedEventArgs e)
+        {
+            var handler = new WeakHandler();
+            ClickEventManager.AddListener(NotifyButton, handler);
+            weakHandlers.Add(handler);
+        }
+
+        private void OnAddGenericWeakHandler(object sender, RoutedEventArgs e)
+        {
+            var handler = new GenericWeakHandler();
+            WeakEventManager<Button, RoutedEventArgs>.AddHandler(NotifyButton, "Click", handler.OnClick);
+            genericWeakHandlers.Add(handler);
+        }
+
+        private void OnClearStrongHandlers(object sender, RoutedEventArgs e)
+        {
+            strongHandlers.Clear();
+        }
+
+        private void OnClearWeakHandlers(object sender, RoutedEventArgs e)
+        {
+            weakHandlers.Clear();
+        }
+
+        private void OnClearGenericWeakHandlers(object sender, RoutedEventArgs e)
+        {
+            genericWeakHandlers.Clear();
+        }
+    }
+}
