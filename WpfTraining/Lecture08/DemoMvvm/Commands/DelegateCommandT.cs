7b93d8aaecd4cc7375811c156013f8eef9d27dac
Pavel <p.fedarovich@sam-solutions.com>
2015-05-25 01:09:47 +0300

    Added DemoMvvm project (Lecture 08).

---
+++ WpfTraining/Lecture08/DemoMvvm/Commands/DelegateCommandT.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Windows.Input;
+
+namespace DemoMvvm
+{
+    public class DelegateCommand<T> : ICommand
+    {
+        private readonly Action<T> execute;
+        private readonly Func<T, bool> canExecute;
+
+        public DelegateCommand(Action<T> execute, Func<T, bool> canExecute = null)
+        {
+            if (execute == null)
+                throw new ArgumentNullException("execute");
+
+            this.execute = execute;
+            this.canExecute = canExecute;
+        }
+
+        public bool CanExecute(object parameter)
+        {
+            return canExecute == null || canExecute((T)parameter);
+        }
+
+        public void Execute(object parameter)
+        {
+            execute((T)parameter);
+        }
+
+        public event EventHandler CanExecuteChanged
+        {
+            add { CommandManager.RequerySuggested += value; }
+            remove { CommandManager.RequerySuggested -= value; }
+        }
+    }
+}
---
+++ WpfTraining/Lecture08/DemoMvvm/ViewModelBase.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.ComponentModel;
+using System.Linq.Expressions;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using JetBrains.Annotations;
+
+namespace DemoMvvm
+{
+    public class ViewModelBase : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        [NotifyPropertyChangedInvocator]
+        protected virtual void RaisePropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChangedEventHandler handler = PropertyChanged;
+            if (handler != null)
+            {
+                handler(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+
+        [NotifyPropertyChangedInvocator]
+        protected virtual void RaisePropertyChanged<T>(Expression<Func<T>> propertyExpresssion)
+        {
+            var propertyName = ExtractPropertyName(propertyExpresssion);
+            RaisePropertyChanged(propertyName);
+        }
+
+        [NotifyPropertyChangedInvocator]
+        protected virtual void RaiseAllPropertiesChanged()
+        {
+            RaisePropertyChanged(string.Empty);
+        }
+
+        private string ExtractPropertyName<T>(Expression<Func<T>> propertyExpression)
+        {
+            if (propertyExpression == null)
+            {
+                throw new ArgumentNullException("propertyExpression");
+            }
+
+            var memberExpression = propertyExpression.Body as MemberExpression;
+            if (memberExpression == null)
+            {
+                throw new ArgumentException("The expression is not a member access expression.", "propertyExpression");
+            }
+
+            var property = memberExpression.Member as PropertyInfo;
+            if (property == null)
+            {
+                throw new ArgumentException("The member access expression does not access  property.", "propertyExpression");
+            }
+
+            if (!property.DeclaringType.IsAssignableFrom(this.GetType()))
+            {
+                throw new ArgumentException("The referenced property belongs to a different type.", "propertyExpression");
+            }
+
+            var getMethod = property.GetGetMethod(true);
+            if (getMethod == null)
+            {
+                // this shouldn't happen - the expression would reject the property before reaching this far
+                throw new ArgumentException("The referenced property does not have a get method.", "propertyExpression");
+            }
+
+            if (getMethod.IsStatic)
+            {
+                throw new ArgumentException("The referenced property is a static property.", "propertyExpression");
+            }
+
+            return memberExpression.Member.Name;
+        }
+    }
+}
