68e4f9dc73ff63980b4c8ceda7220ac106bae4dc
fedarovich <p.fedarovich@sam-solutions.com>
2015-05-25 14:42:31 +0300

    Added EventToCommandBehavior to BlendBehaviors project.

---
+++ WpfTraining/Lecture07/BlendBehaviors/ViewModels/EventToCommandViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System.Windows;
+using System.Windows.Input;
+using Microsoft.Expression.Interactivity.Core;
+
+namespace BlendBehaviors.ViewModels
+{
+    public class EventToCommandViewModel
+    {
+        public EventToCommandViewModel()
+        {
+            ButtonClickCommand = new ActionCommand(() => MessageBox.Show("Button click."));
+            ButtonMouseDownCommand = new ActionCommand(() => MessageBox.Show("Button mouse down."));
+            BorderMouseDownCommand = new ActionCommand(() => MessageBox.Show("Border mouse down."));
+        }
+
+        public ICommand ButtonClickCommand { get; private set; }
+        public ICommand ButtonMouseDownCommand { get; private set; }
+        public ICommand BorderMouseDownCommand { get; private set; }
+    }
+}
