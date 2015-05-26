75705c90662cb913888d99dbcf0790275bd31eeb
fedarovich <p.fedarovich@sam-solutions.com>
2015-05-26 18:59:34 +0300

    Started Weather project for Lecture 08.

---
+++ WpfTraining/Lecture08/Weather/Bootstrapper.cs
@@ -0,0 +1,47 @@
+﻿using System.Reflection;
+using System.Windows;
+using Autofac;
+using DemoMvvm;
+using DemoMvvm.Autofac;
+using DemoMvvm.Services.Implementation;
+using Weather.ViewModels;
+
+namespace Weather
+{
+    public class Bootstrapper : AutofacBootstrapperBase<ShellViewModel>
+    {
+        public Bootstrapper(Application application) : base(application)
+        {
+        }
+
+        protected override void ConfigureContainer(ContainerBuilder builder)
+        {
+            base.ConfigureContainer(builder);
+
+            builder.RegisterType<ViewResolver>()
+                .AsImplementedInterfaces()
+                .SingleInstance();
+            builder.RegisterType<ViewModelResolver>()
+                .AsImplementedInterfaces()
+                .SingleInstance();
+            builder.RegisterType<EventAggregator>()
+                .AsImplementedInterfaces()
+                .SingleInstance();
+            builder.RegisterType<DialogService>()
+                .AsImplementedInterfaces()
+                .SingleInstance();
+            builder.RegisterType<WindowManager>()
+                .AsImplementedInterfaces()
+                .SingleInstance();
+
+            builder.RegisterAssemblyTypes(Assembly.GetExecutingAssembly())
+                .Where(x => x.Name.EndsWith("View"))
+                .AsSelf()
+                .InstancePerDependency();
+            builder.RegisterAssemblyTypes(Assembly.GetExecutingAssembly())
+                .Where(x => x.Name.EndsWith("ViewModel"))
+                .AsSelf()
+                .InstancePerDependency();
+        }
+    }
+}
