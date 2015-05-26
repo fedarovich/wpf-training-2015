ce7f0126c1d86f986446fc6551eb27040b48ef03
fedarovich <fedarovich@localhost>
2015-05-26 18:59:34 +0300

    Started Weather project for Lecture 08.

---
+++ WpfTraining/Lecture08/Weather/ViewModels/ShellViewModel.cs
@@ -0,0 +1,53 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using DemoMvvm;
+using Nito.AsyncEx;
+using OpenWeatherMap;
+
+namespace Weather.ViewModels
+{
+    public class ShellViewModel : ViewModelBase
+    {
+        private string city;
+        private readonly OpenWeatherMapClient client;
+        private INotifyTaskCompletion<CurrentWeatherResponse> currentWeather; 
+
+        public ShellViewModel()
+        {
+            client = new OpenWeatherMapClient();
+            GetCurrentWeatherCommand = new AsyncCommand(GetCurrentWeatherCommandExecute);
+        }
+
+        public string City
+        {
+            get { return city; }
+            set
+            {
+                if (value == city) return;
+                city = value;
+                RaisePropertyChanged();
+            }
+        }
+
+        public INotifyTaskCompletion<CurrentWeatherResponse> CurrentWeather
+        {
+            get { return currentWeather; }
+            set
+            {
+                if (Equals(value, currentWeather)) return;
+                currentWeather = value;
+                RaisePropertyChanged();
+            }
+        }
+
+        public ICommand GetCurrentWeatherCommand { get; private set; }
+
+        private async Task GetCurrentWeatherCommandExecute()
+        {
+            var weatherTask = client.CurrentWeather.GetByName(City);
+            CurrentWeather = NotifyTaskCompletion.Create(weatherTask);
+            await CurrentWeather.Task;
+        }
+    }
+}
