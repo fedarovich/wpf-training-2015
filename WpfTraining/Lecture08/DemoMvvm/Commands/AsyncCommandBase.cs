4ffd4f559fc8737eb2622527949243b6b1c397b5
fedarovich <fedarovich@localhost>
2015-05-26 18:20:31 +0300

    Added async command to DemoMvvm framework.

---
+++ WpfTraining/Lecture08/DemoMvvm/Commands/AsyncCommandBase.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using Nito.AsyncEx;
+
+namespace DemoMvvm
+{
+    public abstract class AsyncCommandBase : IAsyncCommand
+    {
+        #region Fields
+
+        private INotifyTaskCompletion execution;
+
+        #endregion
+
+        #region Properties
+
+        public INotifyTaskCompletion Execution
+        {
+            get { return execution; }
+            protected set
+            {
+                if (!ReferenceEquals(execution, value))
+                {
+                    execution = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+
+        #endregion
+
+        #region Methods
+
+        /// <summary>
+        /// Defines the method to be called when the command is invoked.
+        /// </summary>
+        /// <param name="parameter">
+        /// Data used by the command.  If the command does not require data to be passed, this object can
+        /// be set to null.
+        /// </param>
+        public async void Execute(object parameter)
+        {
+            await ExecuteAsync(parameter);
+        }
+
+        /// <summary>
+        /// Defines the method that determines whether the command can execute in its current state.
+        /// </summary>
+        /// <returns>
+        /// true if this command can be executed; otherwise, false.
+        /// </returns>
+        /// <param name="parameter">
+        /// Data used by the command.  If the command does not require data to be passed, this object can
+        /// be set to null.
+        /// </param>
+        public abstract bool CanExecute(object parameter);
+
+        public abstract Task ExecuteAsync(object parameter);
+
+        protected void RaiseCanExecuteChanged()
+        {
+            CommandManager.InvalidateRequerySuggested();
+        }
+
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            var handler = PropertyChanged;
+            if (handler != null) handler(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        #endregion
+
+        #region Events
+
+        public event EventHandler CanExecuteChanged
+        {
+            add { CommandManager.RequerySuggested += value; }
+            remove { CommandManager.RequerySuggested -= value; }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        #endregion
+    }
+}
