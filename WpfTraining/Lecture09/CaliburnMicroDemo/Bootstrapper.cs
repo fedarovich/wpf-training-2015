cde4a35ffabc4f478329091273fbf92c194da165
Pavel <p.fedarovich@sam-solutions.com>
2015-05-29 00:17:14 +0300

    Added Caliburn.Micro sample.

---
+++ WpfTraining/Lecture09/CaliburnMicroDemo/Bootstrapper.cs
@@ -0,0 +1,44 @@
+﻿using System.Reflection;
+using System.Windows;
+using Autofac;
+using Caliburn.Micro.Autofac;
+using Caliburn.Micro.Extras;
+using CaliburnMicroDemo.ViewModels;
+
+namespace CaliburnMicroDemo
+{
+    public class Bootstrapper : AutofacBootstrapper<ShellViewModel>
+    {
+        public Bootstrapper()
+        {
+            Initialize();
+        }
+
+        protected override void ConfigureContainer(ContainerBuilder builder)
+        {
+            builder.RegisterType<MessageService>()
+                .AsImplementedInterfaces()
+                .SingleInstance();
+            builder.RegisterType<SaveFileService>()
+                .AsImplementedInterfaces()
+                .SingleInstance();
+            builder.RegisterType<OpenFileService>()
+                .AsImplementedInterfaces()
+                .SingleInstance();
+
+            builder.RegisterAssemblyTypes(Assembly.GetExecutingAssembly())
+                .Where(x => x.Name.EndsWith("View"))
+                .AsSelf()
+                .InstancePerDependency();
+            builder.RegisterAssemblyTypes(Assembly.GetExecutingAssembly())
+                .Where(x => x.Name.EndsWith("ViewModel"))
+                .AsSelf()
+                .InstancePerDependency();
+        }
+
+        protected override void OnStartup(object sender, StartupEventArgs e)
+        {
+            DisplayRootViewFor<ShellViewModel>();
+        }
+    }
+}
---
+++ WpfTraining/Lecture09/CaliburnMicroDemo/ViewModels/DocumentViewModel.cs
@@ -0,0 +1,144 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using Caliburn.Micro;
+using Caliburn.Micro.Extras;
+
+namespace CaliburnMicroDemo.ViewModels
+{
+    public class DocumentViewModel : Screen
+    {
+        private readonly IMessageService messageService;
+        private readonly ISaveFileService saveFileService;
+        private readonly IEventAggregator eventAggregator;
+        private static int documentCounter = 0;
+        private string text;
+        private string fullFileName;
+        private bool hasChanges;
+
+        public DocumentViewModel(string path, 
+            IMessageService messageService, 
+            ISaveFileService saveFileService,
+            IEventAggregator eventAggregator)
+        {
+            this.messageService = messageService;
+            this.saveFileService = saveFileService;
+            this.eventAggregator = eventAggregator;
+
+            if (string.IsNullOrWhiteSpace(path))
+            {
+                DisplayName = "Document " + (++documentCounter) + ".txt";
+            }
+            else
+            {
+                fullFileName = path;
+                DisplayName = Path.GetFileName(path);
+                text = File.ReadAllText(path);
+            }
+        }
+
+        public bool HasChanges
+        {
+            get { return hasChanges || string.IsNullOrWhiteSpace(fullFileName); }
+            set
+            {
+                if (value.Equals(hasChanges)) return;
+                hasChanges = value;
+                NotifyOfPropertyChange(string.Empty);
+            }
+        }
+
+        public string Text
+        {
+            get { return text; }
+            set
+            {
+                if (value == text) return;
+                text = value;
+                NotifyOfPropertyChange();
+                HasChanges = true;
+            }
+        }
+
+        public override void CanClose(Action<bool> callback)
+        {
+            if (HasChanges)
+            {
+                var confirmation = messageService.Show(
+                    string.Format("Save changes to file \"{0}\"?", DisplayName),
+                    "Confirmation",
+                    MessageButton.YesNoCancel,
+                    MessageImage.Question);
+
+                if (confirmation == MessageResult.Cancel ||
+                    (confirmation == MessageResult.Yes && !SaveFile()))
+                {
+                    callback(false);
+                    return;
+                }
+            }
+            callback(true);
+        }
+
+        public bool SaveFile()
+        {
+            string path = fullFileName;
+            if (string.IsNullOrWhiteSpace(path))
+            {
+                path = GetSavePath(null, DisplayName);
+                if (string.IsNullOrWhiteSpace(path))
+                    return false;
+            }
+
+            return SaveFileInternal(path);
+        }
+
+        public bool CanSaveFile
+        {
+            get { return HasChanges; }
+        }
+
+        public bool SaveFileAs()
+        {
+            var path = GetSavePath(fullFileName, DisplayName);
+            if (string.IsNullOrWhiteSpace(path))
+                return false;
+
+            return SaveFileInternal(path);
+        }
+
+        private bool SaveFileInternal(string path)
+        {
+            try
+            {
+                File.WriteAllText(path, Text, Encoding.UTF8);
+                fullFileName = path;
+                DisplayName = Path.GetFileName(path);
+                HasChanges = false;
+                NotifyOfPropertyChange(() => HasChanges);
+                return true;
+            }
+            catch (Exception ex)
+            {
+                messageService.Show(ex.Message, "Error", icon: MessageImage.Error);
+                return false;
+            }
+        }
+
+        private string GetSavePath(string initialDirectory, string fileName)
+        {
+            if (string.IsNullOrWhiteSpace(initialDirectory))
+            {
+                initialDirectory = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+            }
+
+            saveFileService.InitialDirectory = initialDirectory;
+            saveFileService.Filter = "Text|*.txt|All Files|*.*";
+            return saveFileService.DetermineFile() ? saveFileService.FileName : null;
+        }
+    }
+}
