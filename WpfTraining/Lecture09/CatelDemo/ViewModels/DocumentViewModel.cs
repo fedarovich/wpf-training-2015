c9ce12aebfe3508b4f547d8d871de200d71a5e32
Pavel <Pavel@localhost>
2015-05-30 14:59:32 +0300

    Added Catel sample.

---
+++ WpfTraining/Lecture09/CatelDemo/ViewModels/DocumentViewModel.cs
@@ -0,0 +1,202 @@
+﻿using System;
+using System.IO;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using Catel.Data;
+using Catel.Messaging;
+using Catel.Services;
+
+namespace CatelDemo.ViewModels
+{
+    using Catel.MVVM;
+
+    /// <summary>
+    /// UserControl view model.
+    /// </summary>
+    public class DocumentViewModel : ViewModelBase
+    {
+        private readonly IMessageService messageService;
+        private readonly ISaveFileService saveFileService;
+        private readonly IMessageMediator messageMediator;
+        private static int documentCounter = 0;
+        private string fullFileName;
+
+        public DocumentViewModel(string path, 
+            IMessageService messageService,
+            ISaveFileService saveFileService,
+            IMessageMediator messageMediator)
+        {
+            this.messageService = messageService;
+            this.saveFileService = saveFileService;
+            this.messageMediator = messageMediator;
+
+            SaveCommand = new TaskCommand(Save, () => HasChanges);
+            SaveAsCommand = new TaskCommand(SaveFileAs);
+
+            if (string.IsNullOrWhiteSpace(path))
+            {
+                Name = "Document " + (++documentCounter) + ".txt";
+                HasChanges = true;
+            }
+            else
+            {
+                fullFileName = path;
+                Name = Path.GetFileName(path);
+                Text = File.ReadAllText(path);
+                HasChanges = false;
+            }
+        }
+
+
+        /// <summary>
+        /// Gets the title of the view model.
+        /// </summary>
+        /// <value>The title.</value>
+        public override string Title { get { return Name; } }
+
+        #region Name property
+
+        /// <summary>
+        /// Gets or sets the Name value.
+        /// </summary>
+        public string Name
+        {
+            get { return GetValue<string>(NameProperty); }
+            set { SetValue(NameProperty, value); }
+        }
+
+        /// <summary>
+        /// Name property data.
+        /// </summary>
+        public static readonly PropertyData NameProperty = RegisterProperty("Name", typeof (string), null,
+            (sender, e) => ((DocumentViewModel)sender).OnNameChanged());
+
+        private void OnNameChanged()
+        {
+            RaisePropertyChanged(() => Title);
+        }
+
+        #endregion
+
+        #region HasChanges property
+
+        /// <summary>
+        /// Gets or sets the HasChanges value.
+        /// </summary>
+        public bool HasChanges
+        {
+            get { return GetValue<bool>(HasChangesProperty); }
+            set { SetValue(HasChangesProperty, value); }
+        }
+
+        /// <summary>
+        /// HasChanges property data.
+        /// </summary>
+        public static readonly PropertyData HasChangesProperty = RegisterProperty("HasChanges", typeof (bool), null,
+            (sender, e) => ((DocumentViewModel) sender).OnHasChangesChanged());
+
+        /// <summary>
+        /// Called when the HasChanges property has changed.
+        /// </summary>
+        private void OnHasChangesChanged()
+        {
+            System.Windows.Input.CommandManager.InvalidateRequerySuggested();
+            SaveCommand.RaiseCanExecuteChanged();
+        }
+
+        #endregion
+
+        #region Text property
+
+        /// <summary>
+        /// Gets or sets the Text value.
+        /// </summary>
+        public string Text
+        {
+            get { return GetValue<string>(TextProperty); }
+            set { SetValue(TextProperty, value); }
+        }
+
+        /// <summary>
+        /// Text property data.
+        /// </summary>
+        public static readonly PropertyData TextProperty = RegisterProperty("Text", typeof (string), null,
+            (sender, e) => ((DocumentViewModel) sender).OnTextChanged());
+
+        /// <summary>
+        /// Called when the Text property has changed.
+        /// </summary>
+        private void OnTextChanged()
+        {
+            HasChanges = true;
+        }
+
+        #endregion
+
+        #region Commands
+
+        public TaskCommand SaveCommand { get; private set; }
+
+        public TaskCommand SaveAsCommand { get; private set; }
+
+        #endregion
+
+        protected async override Task<bool> Save()
+        {
+            string path = fullFileName;
+            if (string.IsNullOrWhiteSpace(path))
+            {
+                path = GetSavePath(null, Name);
+                if (string.IsNullOrWhiteSpace(path))
+                    return false;
+            }
+
+            return await SaveFileInternal(path);
+        }
+
+        private async Task SaveFileAs()
+        {
+            var path = GetSavePath(fullFileName, Name);
+            if (string.IsNullOrWhiteSpace(path))
+                return;
+
+            await SaveFileInternal(path);
+        }
+
+        private async Task<bool> SaveFileInternal(string path)
+        {
+            try
+            {
+                using (var stream = new FileStream(path, FileMode.Create, FileAccess.Write, FileShare.None, 4096, true))
+                using (var writer = new StreamWriter(stream, Encoding.UTF8))
+                {
+                    await writer.WriteAsync(Text);
+                }
+                fullFileName = path;
+                Name = Path.GetFileName(path);
+                HasChanges = false;
+                RaisePropertyChanged(() => HasChanges);
+                return true;
+            }
+            catch (Exception ex)
+            {
+                messageService.ShowError(ex.Message);
+                return false;
+            }
+        }
+
+        private string GetSavePath(string initialDirectory, string fileName)
+        {
+            if (string.IsNullOrWhiteSpace(initialDirectory))
+            {
+                initialDirectory = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+            }
+
+            saveFileService.InitialDirectory = initialDirectory;
+            saveFileService.Filter = "Text|*.txt|All Files|*.*";
+            saveFileService.FileName = fileName;
+            return saveFileService.DetermineFile() ? saveFileService.FileName : null;
+        }
+    }
+}
---
+++ WpfTraining/Lecture09/CatelDemo/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,221 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using Catel.Data;
+using Catel.IoC;
+using Catel.Services;
+
+namespace CatelDemo.ViewModels
+{
+    using Catel.MVVM;
+
+    /// <summary>
+    /// MainWindow view model.
+    /// </summary>
+    public class MainWindowViewModel : ViewModelBase
+    {
+        private readonly ITypeFactory typeFactory;
+        private readonly IOpenFileService openFileService;
+        private readonly IMessageService messageService;
+        private readonly INavigationService navigationService;
+        private readonly IUIVisualizerService uiVisualizerService;
+        private readonly ObservableCollection<DocumentViewModel> documents = new ObservableCollection<DocumentViewModel>();
+        
+        private bool forceClose;
+        private bool isClosing;
+
+        public MainWindowViewModel(ITypeFactory typeFactory, 
+            IOpenFileService openFileService,
+            IMessageService messageService,
+            INavigationService navigationService,
+            IUIVisualizerService uiVisualizerService)
+        {
+            this.typeFactory = typeFactory;
+            this.openFileService = openFileService;
+            this.messageService = messageService;
+            this.navigationService = navigationService;
+            this.uiVisualizerService = uiVisualizerService;
+            NewDocumentCommand = new Command(NewDocumentCommandExecute);
+            OpenDocumentCommand = new Command(OpenDocumentCommandEvecute);
+            CloseDocumentCommand = new TaskCommand<DocumentViewModel>(CloseDocumentCommandExecute, CanCloseDocument);
+            AboutCommand = new Command(AboutCommandExecute);
+            navigationService.ApplicationClosing += OnApplicationClosing;
+        }
+
+        public IList<DocumentViewModel> Documents
+        {
+            get { return documents; }
+        }
+
+        #region SelectedDocument property
+
+        /// <summary>
+        /// Gets or sets the SelectedDocument value.
+        /// </summary>
+        public DocumentViewModel SelectedDocument
+        {
+            get { return GetValue<DocumentViewModel>(SelectedDocumentProperty); }
+            set { SetValue(SelectedDocumentProperty, value); }
+        }
+
+        /// <summary>
+        /// SelectedDocument property data.
+        /// </summary>
+        public static readonly PropertyData SelectedDocumentProperty = RegisterProperty("SelectedDocument", typeof (DocumentViewModel));
+
+        #endregion
+
+        #region Commands
+
+        #region NewDocumentCommand
+
+        public ICommand NewDocumentCommand { get; private set; }
+
+        private void NewDocumentCommandExecute()
+        {
+            var viewModel = CreateDocumentViewModel(string.Empty);
+            documents.Add(viewModel);
+            SelectedDocument = viewModel;
+        }
+
+        #endregion
+
+        #region OpenDocumentCommand
+
+        public ICommand OpenDocumentCommand { get; private set; }
+
+        private void OpenDocumentCommandEvecute()
+        {
+            try
+            {
+                openFileService.InitialDirectory = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+                openFileService.FileName = "";
+                openFileService.Filter = "Text|*.txt|All files|*.*";
+                if (!openFileService.DetermineFile())
+                    return;
+
+                string path = openFileService.FileName;
+                if (string.IsNullOrWhiteSpace(path))
+                    return;
+
+                var document = CreateDocumentViewModel(path);
+                documents.Add(document);
+                SelectedDocument = document;
+            }
+            catch (Exception ex)
+            {
+                messageService.ShowError(ex.Message);
+            }
+        }
+
+        #endregion
+
+        #region CloseDocumentCommand
+
+        public ICommand CloseDocumentCommand { get; private set; }
+
+        private async Task CloseDocumentCommandExecute(DocumentViewModel viewModel)
+        {
+            await CloseDocument(viewModel);
+        }
+
+        private bool CanCloseDocument(DocumentViewModel viewModel)
+        {
+            return viewModel != null && !viewModel.IsClosed;
+        }
+
+        #endregion
+
+        #region AboutCommand
+
+        public ICommand AboutCommand { get; private set; }
+
+        private void AboutCommandExecute()
+        {
+            var viewModel = typeFactory.CreateInstanceWithParametersAndAutoCompletion<AboutViewModel>();
+            uiVisualizerService.ShowDialog(viewModel);
+        }
+
+        #endregion
+
+        #endregion
+
+        private DocumentViewModel CreateDocumentViewModel(string path)
+        {
+            return typeFactory.CreateInstanceWithParametersAndAutoCompletion<DocumentViewModel>(path);
+        }
+
+        private async void OnApplicationClosing(object sender, ApplicationClosingEventArgs e)
+        {
+            if (!forceClose)
+            {
+                e.Cancel = true;
+                if (isClosing)
+                    return;
+
+                isClosing = true;
+                if (await CloseDocuments())
+                {
+                    forceClose = true;
+                    await Task.Yield();
+                    navigationService.CloseApplication();
+                    forceClose = false;
+                }
+                isClosing = false;
+            }
+        }
+
+        private async Task<bool> CloseDocuments()
+        {
+            if (documents.All(x => !x.HasChanges))
+                return true;
+
+            var documentsToClose = documents.ToArray();
+            bool[] closeResults = await Task.WhenAll(documentsToClose.Select(CloseDocument));
+            return closeResults.All(x => x);
+        }
+
+        private async Task<bool> CloseDocument(DocumentViewModel viewModel)
+        {
+            if (viewModel == null)
+                return false;
+
+            bool close;
+            if (viewModel.HasChanges)
+            {
+                var configmationResult = await messageService.Show(
+                    string.Format("Do you want to save changes to the file \"{0}\"?", viewModel.Name),
+                    "Confirmation",
+                    MessageButton.YesNoCancel,
+                    MessageImage.Question);
+
+                switch (configmationResult)
+                {
+                    case MessageResult.Yes:
+                        close = await viewModel.SaveAndCloseViewModel();
+                        break;
+                    case MessageResult.No:
+                        close = await viewModel.CancelAndCloseViewModel();
+                        break;
+                    default:
+                        close = false;
+                        break;
+                }
+            }
+            else
+            {
+                close = await viewModel.CancelAndCloseViewModel();
+            }
+
+            if (close)
+            {
+                Documents.Remove(viewModel);
+            }
+            return close;
+        }
+    }
+}
