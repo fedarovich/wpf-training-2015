47728603214ca5672e449db9954537928b94a93b
fedarovich <p.fedarovich@sam-solutions.com>
2017-02-07 14:54:05 +0300

    Added method to automatically create component resource keys.

--- WpfTraining/Lecture04/ComponentResourceKeys/MyResources.cs
+++ WpfTraining/Lecture04/ComponentResourceKeys/MyResources.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -9,20 +10,21 @@ namespace ComponentResourceKeys
 {
     public static class MyResources
     {
-        public static ResourceKey Brush1Key
+        public static ResourceKey Brush1Key => new ComponentResourceKey(typeof(MyResources), "Brush1");
+
+        public static ResourceKey Brush2Key => new ComponentResourceKey(typeof(MyResources), "Brush2");
+
+        #region CreateKey function (C#5 or later)
+
+        public static ResourceKey Brush3Key => CreateKey();
+
+        public static ResourceKey Brush4Key => CreateKey();
+
+        private static ResourceKey CreateKey([CallerMemberName] string name = null)
         {
-            get
-            {
-                return new ComponentResourceKey(typeof(MyResources), "Brush1");
-            }
+            return new ComponentResourceKey(typeof(MyResources), name.Substring(0, name.Length - 3));
         }
 
-        public static ResourceKey Brush2Key
-        {
-            get
-            {
-                return new ComponentResourceKey(typeof(MyResources), "Brush2");
-            }
-        }
+        #endregion
     }
 }
