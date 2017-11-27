5a6ad7f7cf25adb5d8a995dac1248e67b5cabd26
fedarovich <p.fedarovich@sam-solutions.com>
2017-11-27 15:30:57 +0300

    Added project for demonstration of control templates.

---
+++ WpfTraining/Lecture05/ControlTemplates/ClearableTextBox.cs
@@ -0,0 +1,56 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace ControlTemplates
+{
+    [TemplatePart(Name = ClearButtonName, Type = typeof(Button))]
+    [StyleTypedProperty(Property = nameof(ButtonStyle), StyleTargetType = typeof(Button))]
+    public class ClearableTextBox : TextBox
+    {
+        private const string ClearButtonName = "PART_ClearButton";
+
+        private Button _clearButton;
+
+        #region ButtonStyle
+
+        public static readonly DependencyProperty ButtonStyleProperty = DependencyProperty.Register(
+            "ButtonStyle", typeof(Style), typeof(ClearableTextBox), new PropertyMetadata(null));
+
+        public Style ButtonStyle
+        {
+            get { return (Style) GetValue(ButtonStyleProperty); }
+            set { SetValue(ButtonStyleProperty, value); }
+        }
+
+        #endregion
+
+        static ClearableTextBox()
+        {
+            DefaultStyleKeyProperty.OverrideMetadata(
+                typeof(ClearableTextBox), 
+                new FrameworkPropertyMetadata(typeof(ClearableTextBox)));
+        }
+
+        public override void OnApplyTemplate()
+        {
+            base.OnApplyTemplate();
+
+            if (_clearButton != null)
+            {
+                _clearButton.Click -= OnClearButtonClick;
+            }
+
+            _clearButton = GetTemplateChild(ClearButtonName) as Button;
+
+            if (_clearButton != null)
+            {
+                _clearButton.Click += OnClearButtonClick;
+            }
+        }
+
+        private void OnClearButtonClick(object sender, RoutedEventArgs e)
+        {
+            this.Clear();
+        }
+    }
+}
